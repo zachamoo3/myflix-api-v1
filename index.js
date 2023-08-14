d31e5749bf7d9daeb6e8721f945b3999d27734a2
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-14 15:52:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const Users = Models.User;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', {
     useNewUrlParser: true,
