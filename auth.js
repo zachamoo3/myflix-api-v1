16a92d928cee2a7ffb13871797a1e7ab8cc9aacb
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-14 15:27:18 -0500

    typo

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const jstSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
