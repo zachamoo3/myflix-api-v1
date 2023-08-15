77979bc9191e7a3ed65931498b51342e741a3881
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-14 20:32:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,23 @@ mongoose.connect('mongodb://localhost:27017/myFlixDB', {
 });
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-let auth = require('./auth')(app);
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            // If a specific origin isn't found on the list of allowed origins
+            let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
+let auth = require('./auth.js')(app);
 const passport = require('passport');
-require('./passport');
+require('./passport.js');
 
 // setup Logging
 const accessLogStream = fs.createWriteStream( // create a write stream
