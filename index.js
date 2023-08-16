f4709533f00b875e7c3f170ca962ad143b98f878
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-16 16:18:46 -0500

    Setup listening for Heroku

--- index.js
+++ index.js
@@ -419,9 +419,10 @@ app.delete('/users/:Username',
 
 
 // Listen for requests
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-});
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+    console.log('Listening on Port ' + port);
+})
 
 // setup Error Handling
 app.use((err, req, res, next) => {
