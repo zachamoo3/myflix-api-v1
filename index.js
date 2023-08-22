87b6fe458ccdef5206fe602b0762c93883040609
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-22 11:11:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', '*'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
     origin: (origin, callback) => {
