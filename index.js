94782d05f28135301f5fd2ab466c6a2b03519d36
Zachary Moore <zachary.d.moore184@gmail.com>
2024-01-22 22:08:56 -0600

    Revert "Update index.js"
    This reverts commit 78177645a3b5b7326e9f4058761555b8af9d14a6.

--- index.js
+++ index.js
@@ -409,16 +409,16 @@ app.delete('/users/:Username',
             .then((user) => {
                 if (!user) {
                     res.status(400)
-                        .send(req.params.Username, ' was not found');
+                        .send(req.params.Username + ' was not found');
                 } else {
                     res.status(200)
-                        .send(req.params.Username, ' was deleted.');
+                        .send(req.params.Username + ' was deleted.');
                 }
             })
             .catch((err) => {
                 console.error(err);
                 res.status(500)
-                    .send('Error: ', err);
+                    .send('Error: ' + err);
             });
     }
 );
